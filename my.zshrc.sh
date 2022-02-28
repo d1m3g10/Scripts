cec994721344901f10aaa8fffd8f346cd2bce660
d1m3g10 <92630993+d1m3g10@users.noreply.github.com>
2022-02-28 14:15:04 +0100

    Create my.zshrc.sh
    Algunas de las funciones personalizadas que contiene mi zshrc

---
+++ my.zshrc.sh
@@ -0,0 +1,29 @@
+#Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
+#ExtractPorts
+function extractPorts(){
+	ip_maquina=$(cat allPorts | grep -oP '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}' | sort -u)
+	puertos_ab=$(cat allPorts | grep -oP '\d{1,5}/open' | awk '{print $1}' FS="/" | xargs | tr ' ' ',')
+	echo -e "\n\t${greenColour}[-] Puertos abiertos de la máquina "$ip_maquina": ${endColour}${grayColour}"$puertos_ab"${endColour}"
+	echo $puertos_ab | tr -d '\n' | xclip -sel clip
+	echo -e "\t${greenColour}[-] Puertos copiados en la clipboard. ${endColourColour}"
+
+}
+
+#Kill python http server on the in port 80
+function killPythonServerHTTP80(){
+	kill -9 $(lsof -ti tcp:80)
+}
+
+#Custom Aliases
+alias cat='/usr/bin/bat'
+alias catn='/usr/bin/cat'
+alias catnl='/usr/bin/bat --paging=never'

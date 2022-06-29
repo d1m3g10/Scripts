b4b2f0fba0a28a9b794147139ff8be8cf59170e2
d1m3g10 <92630993+d1m3g10@users.noreply.github.com>
2022-06-30 01:16:28 +0200

    Update my.zshrc.sh

--- my.zshrc.sh
+++ my.zshrc.sh
@@ -8,6 +8,14 @@ purpleColour="\e[0;35m\033[1m"
 turquoiseColour="\e[0;36m\033[1m"
 grayColour="\e[0;37m\033[1m"
 
+
+#Custom Aliases
+alias cat='/usr/bin/bat'
+alias catn='/usr/bin/cat'
+alias catnl='/usr/bin/bat --paging=never'
+alias lk='lsd -l --group-dirs=first'
+
+
 #ExtractPorts
 function extractPorts(){
 	ip_maquina=$(cat allPorts | grep -oP '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}' | sort -u)
@@ -23,7 +31,20 @@ function killPythonServerHTTP80(){
 	kill -9 $(lsof -ti tcp:80)
 }
 
-#Custom Aliases
-alias cat='/usr/bin/bat'
-alias catn='/usr/bin/cat'
-alias catnl='/usr/bin/bat --paging=never'
+
+function selectorVentana(){
+        xprop WM_CLASS
+}
+
+#Volvemos a la pantalla de bloqueo sin salirnos de la sesión del usuario
+function screenlock(){
+        dm-tool lock
+}
+
+function upvup(){
+        nmcli con up VPN\ a\ UPV -a
+}
+
+function upvdown(){
+        nmcli con down VPN\ a\ UPV -a
+}
